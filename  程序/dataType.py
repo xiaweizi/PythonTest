042afc6f183309d3944c33269736e6a5f08d7a0b
xiaweizi <1012126908@qq.com>
2017-12-29 17:00:40 +0800

    添加「数据类型和变量」

---
+++  程序/dataType.py
@@ -0,0 +1,25 @@
+name = 'I\'m \"OK\"!'
+print('name', name)
+
+print('\\\t\\')
+print(r'\\\t\\')
+print('----------------------------------------')
+
+print('''line1
+line2
+line3''')
+print('----------------------------------------')
+
+print(True)
+flag = 2 > 1
+print(flag)
+print(True or False)
+print(True and False)
+print(not True)
+print('----------------------------------------')
+
+age = 20
+if age > 10:
+	print(True)
+else:
+	print(False)
