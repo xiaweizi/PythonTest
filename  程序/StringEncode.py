5b1a792936303b47d1bd691d1a9de7c8d86e5073
xiaweizi <1012126908@qq.com>
2017-12-29 17:33:48 +0800

    添加「字符串和编码」

---
+++  程序/StringEncode.py
@@ -0,0 +1,28 @@
+#1 /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('我是下位子ddd')
+
+print(ord('A'))
+print(ord('D'))
+print(chr(65))
+print(chr(25))
+
+print(b'ABC')
+
+a = 'ABC'
+b = b'ABC'
+
+print(a.decode('ascii'))
+print(b.decode('ascii'))
+
+print(len('ABC'))
+print(len('我'))
+
+
+print('Integer: %d' % 2)
+print('Float: %f' % 1.2)
+print('String: %s' % 'xiaweizi')
+print('16进制: %x' % 0x22)
+
+print('升值了 %d %%' % 20)
