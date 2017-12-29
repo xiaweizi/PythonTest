0ca828c2420e88438f45f26ad32e4a610384c500
xiaweizi <1012126908@qq.com>
2017-12-29 11:13:57 +0800

    添加测试程序

---  程序/hello.py
+++  程序/hello.py
@@ -1,2 +1,8 @@
-a = b'dafd'
-print(a)
+#1 /usr/bin/env python3
+# -*- coding: utf-8 -*-
+name = 'xiaweizi'
+print('name', name)
+age = 12
+print('age',age)
+sex = input('请输入您的性别：')
+print('sex:', sex)
