3559e36267d13157ddfe0adc708c6c35c8666daf
xiaweizi <1012126908@qq.com>
2017-12-31 10:14:55 +0800

    添加「循环」程序

---
+++  程序/Circle.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+datas = {'a', 'b', 'c', 'd'}
+for data in datas:
+	print(data)
+
+nums = {1,2,3,4,5}
+sum = 0
+for num in nums:
+	sum += num
+print(sum)
+
+print(range(10))
+print(list(range(10)))
+
+## 算出 1 到 100 的和
+sum = 0
+for num in range(101):
+	sum += num
+print(sum)
+
+# While
+sum = 0 
+n = 0
+while n <= 100:
+	sum += n
+	n += 1
+print(sum)
+
+# 1 - 10 找出第一个偶数
+n = 1
+while n <= 10:
+	n += 1
+	if n % 2 == 0:
+		print(n)
+		continue
+	
