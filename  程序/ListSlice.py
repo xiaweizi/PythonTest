a91429d94a36ae9dca8cbab4d53de786e21a02ed
xiaweizi <1012126908@qq.com>
2018-01-04 14:58:08 +0800

    添加「切片」程序和代码

---
+++  程序/ListSlice.py
@@ -0,0 +1,17 @@
+L = [1,2,3,4,5,6,7,8,9,10]
+print(L[0:4])
+print(L[1:3])
+print(L[:3])
+print(L[3:])
+print(L[::])
+print(L[0:4:2])
+print(L[2::3])
+print(L[2::3])
+
+print('-----------------------------------------------------')
+
+print(L[-5:])
+
+string = "abcdefg"
+
+print(string[4:])
