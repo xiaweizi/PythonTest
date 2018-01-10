c68974723f918897a40c6f890ea4236932674d6b
xiaweizi <1012126908@qq.com>
2018-01-10 15:11:06 +0800

    添加「@property」程序

---
+++  程序/面向对象编程/class5.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    __slots__ = ('name',)
+
+def func(obj, name):
+    obj.name = name
+
+student = Student()
+func(student, 12)
+
+print student.name
+
+class Screen(object):
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @width.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        return self._width * self._height
+
+s = Screen()
+s.width = 1024
+s.height = 768
+s.resolution  =12
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
