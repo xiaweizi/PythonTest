fb06aa204fde836184b742fa777dc6d8ce3bd503
xiaweizi <1012126908@qq.com>
2018-01-10 13:15:02 +0800

    添加「类的集成和多态」程序

---
+++  程序/面向对象编程/class2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+    def run(self):
+        print 'Animal is running...'
+
+class Cat(Animal):
+    def run(self):
+        print 'Cat is running...'
+
+def test(animal):
+    animal.run()
+
+animal = Animal()
+animal = Cat()
+test(animal)
+
+class People(object):
+    def __run(self):
+        print 'People is running...'
+
+def test1(people):
+    people._People__run()
+
+people = People()
+test1(people)
